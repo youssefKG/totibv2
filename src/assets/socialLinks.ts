d27350659282c4d68a8685344612954d2b22f972
youssefKG <yousseftaoussi2003@gmail.com>
2025-04-13 09:24:31 +0100

    .

---
+++ src/assets/socialLinks.ts
@@ -0,0 +1,8 @@
+const socialLinks = {
+  github: "https://github.com/youssefKG",
+  linkedin: "https://www.linkedin.com/in/youssef-taoussi-0081a7264/",
+  facebook: "https://www.facebook.com/youssef.taoussi.184/",
+  instagram: "",
+};
+
+export default socialLinks;
